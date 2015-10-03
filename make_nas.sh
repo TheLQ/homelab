8e83108b036321632cc71fd29672892f7f5cfb94
Leon Blakey <leon.m.blakey@gmail.com>
2015-10-03 19:27:51 -0400

    New server nas

---
+++ make_nas.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+source common.sh
+CONT_NAME=qnas
+
+#TODO: Depends on qbr0 existing on host
+vm_make $CONT_NAME 111 qbr0
+
+# add the lxc root directory for debugging purposes
+mkdir $VM_FS/lxcroot
+echo lxc.mount.entry=/var/lib/lxc/ $VM_FS/lxcroot none bind 0 0 >> $VM_ROOT/config
+
+vm_start_first $CONT_NAME
+
+# setup NFS exports
+lxc-attach -n $CONT_NAME -- apt-get install nfs-kernel-server -y
+rm $VM_FS/etc/exports
+ln -s $DATA_DIR/configs/nas/exports $VM_FS/etc/exports
+lxc-attach -n $CONT_NAME -- service nfs-kernel-server restart -y
+
+# setup samba
+lxc-attach -n $CONT_NAME -- apt-get install samba -y
+rm $VM_FS/etc/samba/smb.conf
+ln -s $DATA_DIR/configs/nas/smb.conf $VM_FS/etc/samba/smb.conf
+# tmp
+lxc-attach -n $CONT_NAME -- $DATA_DIR/mount-quackdrive.sh
+lxc-attach -n $CONT_NAME -- service smbd restart
