7903faca05e34a6cc729141a646dfdd25e40680c
Leon Blakey <leon.m.blakey@gmail.com>
2015-10-20 00:20:47 -0400

    initial desktop config

---
+++ desktop/monitor-left-vertical.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output DP2 --off --output DP1 --primary --mode 1920x1080 --pos 0x272 --rotate normal --output HDMI2 --off --output HDMI1 --off --output VGA1 --mode 1440x900 --pos 1920x0 --rotate left
