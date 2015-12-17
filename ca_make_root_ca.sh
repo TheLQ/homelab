2803b3ff91e3bfe31b8a5f835f3bb04ffe834e2c
Leon Blakey <leon.m.blakey@gmail.com>
2015-12-16 23:35:31 -0500

    Moved CA handling to ca_common.sh

--- ca_make_root_ca.sh
+++ ca_make_root_ca.sh
@@ -11,8 +11,16 @@ if [[ ! -d "$SECURE_DIR/certs" ]]; then
 fi
 
 source ca_common.sh
-ca_init_vm
 
 # see http://datacenteroverlords.com/2012/03/01/creating-your-own-ssl-certificate-authority/
+if [[ -f $CA_KEY ]]; then
+	echo "Key file $CA_KEY already exists"
+	exit 2
+fi
+if [[ -f $CA_CERT ]]; then
+	echo "Certificate $CA_KEY already exists"
+	exit 2
+fi
+
 openssl genrsa -des3 -out $CA_KEY 2048
 openssl req -x509 -new -nodes -key $CA_KEY -days 1024 -out $CA_CERT
