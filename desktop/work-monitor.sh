f806e55851ab9711c48b14d49015694f1a16899a
Leon Blakey <leon.m.blakey@gmail.com>
2015-12-17 22:10:17 -0500

    Various i3config tweaks

--- desktop/work-monitor.sh
+++ desktop/work-monitor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VGA-0 --mode 1920x1080 --pos 0x0 --rotate left --output DVI-0 --primary --mode 1920x1080 --pos 1080x0 --rotate left --output HDMI-0 --off
+xrandr --output VGA-0 --primary --mode 1920x1080 --pos 0x0 --rotate left --output DVI-0 --mode 1920x1080 --pos 1080x0 --rotate left --output HDMI-0 --off
